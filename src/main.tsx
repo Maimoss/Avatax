78e2a1463e7230455c4e746642ccf8ae67263f08
Krish Bharadwaj <krishbharadwaj50@gmail.com>
2025-11-21 19:24:12 +0530

    Added SEO, Pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AvatarProvider } from "./context/AvatarContext.tsx";
-import { ColorProvider } from "./context/ColorContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AvatarProvider>
-      <ColorProvider>
-        <App />
-      </ColorProvider>
-    </AvatarProvider>
+    <App />
   </StrictMode>
 );
